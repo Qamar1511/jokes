1aee038deace7d58a3acd90a2fa3c07ab8b6f54e
Qamar1511 <khanqamar485@gmail.com>
2023-03-28 15:32:27 +0530

    some code is removed

---
+++ script.js
@@ -0,0 +1,495 @@
+let myjokes = [
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Debugging: Removing the needles from the haystack.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 40,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "I didn't vaccinate my 10 kids and the one that survived is fine!",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 96,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "The generation of random numbers is too important to be left to chance.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 39,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A byte walks into a bar looking miserable.\nThe bartender asks it: \"What's wrong buddy?\"\n\"Parity error.\" it replies. \n\"Ah that makes sense, I thought you looked a bit off.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 24,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "I have these weird muscle spasms in my gluteus maximus.\nI figured out from my doctor that everything was alright:\nHe said \"Weird flex, butt okay.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 82,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Your momma is so fat, you need to switch to NTFS to store a picture of her.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": true
+    },
+    "id": 55,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Your mama's so FAT she can't save files bigger than 4GB.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": true
+    },
+    "id": 9,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "A man is smoking a cigarette and blowing smoke rings into the air. His girlfriend becomes irritated with the smoke and says \"Can't you see the warning on the cigarette pack? Smoking is hazardous to your health!\" to which the man replies, \"I am a programmer.  We don't worry about warnings; we only worry about errors.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 38,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "// This line doesn't actually do anything, but the code stops working when I delete it.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 12,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Today I learned that changing random stuff until your program works is \"hacky\" and a \"bad coding practice\" but if you do it fast enough it's \"Machine Learning\" and pays 4x your current salary.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 33,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "Stop being homophobic and rude to the LGBTQ+ community. You should be thanking them for saving us plenty of room in heaven.",
+    "flags": {
+      "nsfw": false,
+      "religious": true,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 114,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "Women are like KFC, once you're done with the breasts and thighs, you just have a greasy box to put your bone in.",
+    "flags": {
+      "nsfw": true,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": true,
+      "explicit": true
+    },
+    "id": 149,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "Being a self-taught developer is almost the same as being a cut neck chicken because you have no sense of direction in the beginning.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 184,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "Today, my son asked \"Can I have a book mark?\" and I burst into tears.\n11 years old and he still doesn't know my name is Brian.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 200,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Programming",
+    "type": "single",
+    "joke": "The six stages of debugging:\n1. That can't happen.\n2. That doesn't happen on my machine.\n3. That shouldn't happen.\n4. Why does that happen?\n5. Oh, I see.\n6. How did that ever work?",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 123,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "Schrödinger's cat walks into a bar and doesn't.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 197,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "Doctor: \"I have some news about your baby.\"\nParents: \"Don't tell us the gender, we want to keep it a surprise.\"\nDoctor: \"Oh I get it, you're those type of people. Okay, well IT is not breathing.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 138,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "Relationship Status: just tried to reach for my dog's paw and he pulled it away so I pretended I was reaching for the remote.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "racist": false,
+      "sexist": false,
+      "political": false,
+      "explicit": false
+    },
+    "id": 195,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "Two fish in a tank. One turns to the other and says, \"Do you know how to drive this thing?\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 185,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "My little daughter came to me all excited, saying \"Daddy! Daddy! Guess how old I'll be in June!\"\n\"Oh I don't know princess, why don't you tell me?\" I said. She gave me a huge smile and held up four fingers.\nIt's now three hours later, police have joined in and she still won't say where she got them.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 165,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "I was struggling to figure out how lightning works, but then it struck me.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 220,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "Never date a baker. They're too kneady.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "safe": true,
+    "id": 271,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "I hate double standards. Burn a body at a crematorium, you're \"being a respectful friend.\" Do it at home and you're \"destroying evidence.\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": true
+    },
+    "safe": false,
+    "id": 274,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "Hey girl are you a school? Because I want to shoot some kids up inside of you.",
+    "flags": {
+      "nsfw": true,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": true
+    },
+    "safe": false,
+    "id": 269,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "My wife is really mad at the fact that I have no sense of direction.\nSo I packed up my stuff and right.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 201,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "I have a fish that can breakdance! Only for 20 seconds though, and only once.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 237,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "I have a joke about trickle down economics, but 99% of you will never get it.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 238,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Misc",
+    "type": "single",
+    "joke": "A perfectionist walked into a bar... apparently, the bar was not set high enough.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "safe": true,
+    "id": 288,
+    "lang": "en"
+  },
+  {
+    "category": "Pun",
+    "type": "single",
+    "joke": "I was reading a great book about an immortal dog the other day. It was impossible to put down.",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 236,
+    "safe": true,
+    "lang": "en"
+  },
+  {
+    "category": "Dark",
+    "type": "single",
+    "joke": "I'll never forget my Granddad's last words to me just before he died. \"Are you still holding the ladder?\"",
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 208,
+    "safe": false,
+    "lang": "en"
+  },
+  {
+    "error": true,
+    "internalError": false,
+    "code": 106,
+    "message": "No matching joke found",
+    "causedBy": [
+      "No jokes were found that match your provided filter(s)."
+    ],
+    "additionalInfo": "The specified ID range is invalid. Got: \"301 to 400\" but max possible ID range is: \"0-319\".",
+    "timestamp": 1679991856104
+  }
+]
+let index = Math.floor(Math.random() * (myjokes.length - 1))
+joke.innerHTML = myjokes[index].joke
